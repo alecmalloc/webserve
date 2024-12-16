81fefbc96182456b702ec1f9606699020d97a811
Mo <mb_code@proton.me>
2024-12-16 16:32:06 +0100

    first structure

---
+++ inc/Config.hpp
@@ -0,0 +1,26 @@
+#ifndef CONFIG_HPP
+# define CONFIG_HTTP
+
+class	Config{
+
+	public:
+		//Constructors
+		Config( void );
+		Config( const Config& og );
+		~Config( void );
+
+		//operator overloads
+		Config&	operator =( const Config& og );
+
+		//getter funcitons
+		
+		//setter functions 
+
+		//public member functions
+
+	private:
+		
+	
+};
+
+#endif
