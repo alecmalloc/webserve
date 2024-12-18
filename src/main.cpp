3e750c54e1478cfe9950af5a9dbfd5194c25e913
alecmalloc <aeastman@student.42berlin.de>
2024-12-18 11:05:17 +0100

    commented out main

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@ int main(int argc, char**argv) {
     (void)argc;
     (void)argv;
     
-    // *** testing for http-request *** DELETE
-    #include <fcntl.h>
-    #include <unistd.h>
-    int fd = open("get.example", O_RDONLY);
-    if (fd < 0)
-        return -1;
-    try {
-        HttpRequest http(fd);
-    }
-    catch (std::runtime_error &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    // *** TESTING FOR HTTP_REQUEST
+    // #include <fcntl.h>
+    // #include <unistd.h>
+    // int fd = open("get.example", O_RDONLY);
+    // if (fd < 0)
+    //     return -1;
+    // try {
+    //     HttpRequest http(fd);
+    // }
+    // catch (std::runtime_error &e) {
+    //     std::cerr << "Error: " << e.what() << std::endl;
+    // }
 
     // Server server;
     // server.start();
