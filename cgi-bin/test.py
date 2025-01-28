2c0b45986e2ed9afb5aa63f2c21a1d31514a9a78
mo <moba42@proton.me>
2025-01-28 12:24:12 +0100

    blocked out cgi test

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 import os, sys
 
-print("Content-Type: text/plain\n")
-print("CGI Script Executed")
-print("Request Method:", os.environ.get("REQUEST_METHOD", ""))
-print("Query String:", os.environ.get("QUERY_STRING", ""))
-print("Content-Type:", os.environ.get("CONTENT_TYPE", ""))
-print("Body:", sys.stdin.read())
-
 while True:
     sys.stdin.read();
