c1610d5801fce07d8d01a2c46ad5828b0ce65c35
mo <moba42@proton.me>
2025-04-16 13:02:46 +0200

    added Redirect Directive to ServerConf

--- inc/ServerConf.hpp
+++ inc/ServerConf.hpp
@@ -30,6 +30,7 @@ class ServerConf {
 		const std::vector< std::string >		getIndex( void ) const;
 		bool						getChunkedTransfer( void ) const;
 		size_t						getChunkSize( void ) const;
+		const std::map< std::string, std::string >	getAllowedRedirects( void ) const;
 
 		//setter functions
 		void	setIpPort( std::string ip, int port );
@@ -42,6 +43,7 @@ class ServerConf {
 		void	setIndex( std::string );
 		void	setChunkedTransfer( bool chunked );
  		void	setChunkSize( size_t size );
+		void	setAllowedRedirects( std::string, std::string );
 
 		//member functions
 		void	checkAccess( void );
@@ -76,6 +78,9 @@ class ServerConf {
 	
 	        //size of each chunk
 		size_t				_chunkSize;
+		
+		//store http redirects
+		std::map< std::string, std::string >	_allowedRedirects;
 };
 
 #endif
